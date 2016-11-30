956eb8d765ea2215fc96e84d7a55dbfb4be9ffac
chikara <chenhongtu@51xianqu.net>
2016-11-30 10:15:07 +0800

    warning function

--- src/utils/warning.js
+++ src/utils/warning.js
@@ -5,5 +5,7 @@
 export default function warning(message) {
 	if (typeof console !== 'undefined' && typeof console.error === 'function') {
 		console.error(message)
+	} else {
+		throw new Error(message)
 	}
 }
