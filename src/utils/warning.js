383461e30331d08fbb8cf424438e46199c6436b2
chikara <chenhongtu@51xianqu.net>
2016-12-03 13:16:35 +0800

    warning

--- src/utils/warning.js
+++ src/utils/warning.js
@@ -3,9 +3,17 @@
  * @param  {String} message
  */
 export default function warning(message) {
-	if (typeof console !== 'undefined' && typeof console.error === 'function') {
-		console.error(message)
-	} else {
-		throw new Error(message)
-	}
+  /* eslint-disable no-console */
+  if (typeof console !== 'undefined' && typeof console.error === 'function') {
+    console.error(message)
+  }
+  /* eslint-enable no-console */
+  try {
+    // This error was thrown as a convenience so that if you enable
+    // "break on all exceptions" in your console,
+    // it would pause the execution at this line.
+    throw new Error(message)
+    /* eslint-disable no-empty */
+  } catch (e) {}
+  /* eslint-enable no-empty */
 }
